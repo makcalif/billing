d4553ddbcb6248bc92ccc7688c045e031e98ecf0
mumtaz <makcalif@yahoo.com>
2024-09-29 21:39:56 -0500

    s3 upload working

--- src/pages/Bank.js
+++ src/pages/Bank.js
@@ -1,44 +1,18 @@
-import React, { useState } from 'react';
-// Import the correct methods from @aws-amplify/storage
-import { uploadData } from '@aws-amplify/storage';
+// import React, { useState } from 'react';
+// // Import uploadData from @aws-amplify/storage
+// import { uploadData } from '@aws-amplify/storage';
+import { FileUploader } from '@aws-amplify/ui-react-storage';
 
 function Bank() {
-  const [file, setFile] = useState(null);
-  const [uploadStatus, setUploadStatus] = useState('');
-
-  const handleFileChange = (event) => {
-    const uploadedFile = event.target.files[0];
-    setFile(uploadedFile);
-  };
-
-  const uploadFileToS3 = async () => {
-    if (!file) {
-      setUploadStatus('Please choose a file first.');
-      return;
-    }
-
-    try {
-      const result = await uploadData(file.name, file, {
-        contentType: file.type,
-      });
-      setUploadStatus(`File uploaded successfully: ${result}`);
-    } catch (error) {
-      setUploadStatus(`File upload failed: ${error.message}`);
-    }
-  };
+   
 
   return (
-    <section>
-      <h2>Bank</h2>
-      <p>Learn more about our banking services!</p>
-
-      <h3>Upload a File</h3>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={uploadFileToS3}>Upload to S3</button>
-
-      {uploadStatus && <p>{uploadStatus}</p>}
-    </section>
+    <FileUploader
+      acceptedFileTypes={['image/*']}
+      path="public/"
+      maxFileCount={1}
+      isResumable
+    />
   );
-}
-
+};
 export default Bank;
